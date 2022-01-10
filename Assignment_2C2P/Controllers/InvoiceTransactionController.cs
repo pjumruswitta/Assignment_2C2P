f77a390a4a5c52dfc55be4d47866a4726bde33b4
PJay <pea@1stcraft.com>
2022-01-11 00:44:29 +0700

    add invoice controller for try get value

---
+++ Assignment_2C2P/Controllers/InvoiceTransactionController.cs
@@ -0,0 +1,26 @@
+﻿using Assignment_2C2P.Models;
+using Assignment_2C2P.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_2C2P.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class InvoiceTransactionController : ControllerBase
+    {
+        private IInvoiceServices _invoiceService; 
+        public InvoiceTransactionController(IInvoiceServices invoiceService)
+        {
+            this._invoiceService = invoiceService;
+        }
+
+        [HttpGet]
+        public async Task<List<InvoiceTransactionResponse>> Get()
+        {
+            return await _invoiceService.GetInvoices();
+        }
+    }
+}
